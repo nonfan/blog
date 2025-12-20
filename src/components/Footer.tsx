bf608ea590a21641eced815d3064a9d34cd44b81
win-mofan <win@mofan>
2025-12-20 20:13:09 +0800

    style: fix footer to stick to bottom and use gray border

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,13 +10,13 @@ export default function Footer() {
 
   return (
     <footer className="site-footer">
+      <div className="footer-bar" />
       <div className="footer-content">
         <span className="footer-license">基于 MIT 许可发布</span>
         <span className="footer-copyright">
           Copyright © {copyright} <a href="#" className="footer-author">{author?.toUpperCase()}</a>
         </span>
       </div>
-      <div className="footer-bar" />
     </footer>
   )
 }
